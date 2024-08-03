4c3e49183bf1dbc7ee9375faa88f4358e73959db
Nadelele <nadele@gmail.com>
2024-08-03 21:18:11 +0300

    added executor

--- src/main/java/telran/net/TcpServer.java
+++ src/main/java/telran/net/TcpServer.java
@@ -1,40 +1,59 @@
 package telran.net;
+
 import java.net.*;
+import java.util.concurrent.*;
+
+
 import static telran.net.TcpConfigurationProperties.*;
-public class TcpServer implements Runnable{
+
+public class TcpServer implements Runnable {
 	Protocol protocol;
 	int port;
 	boolean running = true;
+	ExecutorService executor;
+
 	public TcpServer(Protocol protocol, int port) {
 		this.protocol = protocol;
 		this.port = port;
+		this.executor = Executors.newFixedThreadPool(getNumberOfThreads());
+
 	}
+
 	public void shutdown() {
-		running = false;
+		executor.shutdown();
+		try {
+			executor.awaitTermination(10, TimeUnit.SECONDS);
+			running = false;
+		} catch (InterruptedException e) {
+
+		}
+		
 	}
+
+	private int getNumberOfThreads() {
+		Runtime runtime = Runtime.getRuntime();
+		return runtime.availableProcessors();
+	}
+
 	public void run() {
-		try(ServerSocket serverSocket = new ServerSocket(port)){
-			//TODO using ServerSocket method setSoTimeout 
+		try (ServerSocket serverSocket = new ServerSocket(port)) {
 			System.out.println("Server is listening on port " + port);
 			serverSocket.setSoTimeout(SOCKET_TIMEOUT);
-			while(running) {
+			while (running) {
 				try {
 					Socket socket = serverSocket.accept();
 
-					TcpClientServerSession session =
-							new TcpClientServerSession(socket, protocol, this);
-					session.start();
+					TcpClientServerSession session = new TcpClientServerSession(socket, protocol, this);
+					executor.execute(session);
+
 				} catch (SocketTimeoutException e) {
-					
+
 				}
-				
 			}
-			
-				
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 }
